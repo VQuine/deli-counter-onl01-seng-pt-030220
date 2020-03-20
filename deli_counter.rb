413d7e64db5dddc802a68ef2d707d4c9ab1b6f5d
Kimberly Aviles <01010110.v@gmail.com>
2020-03-20 22:07:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,16 @@
 array = []
 
-def line(array)
-  if array.count == 0
-    puts "The line is currently empty."
-  else
-    puts array.each_with_index{|element, index| "#{index.to_i + 1}. #{element}"}
-end
+# def line(array)
+#   if array.count == 0
+#     puts "The line is currently empty."
+#   else
+#     puts array.each_with_index{|element, index| "#{index.to_i + 1}. #{element}"}
+# end
 
+def take_a_number(name_string_array, name_string)
+  name_string_array << name_string
+  "Welcome, #{name_string}.  You are number #{name_string_array.length} in line."
+end
 # def take_a_number(array, name)
 #   "Welcome, #{name}. You are number " + array.each_with_index{|element, index| index + 1}.to_s + " in line."
 # end
