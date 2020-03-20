252b5e98d12d5926cb4f7cdffc4894b5e00c91e3
Kimberly Aviles <01010110.v@gmail.com>
2020-03-20 03:55:22 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,12 @@
 array = []
 
-def take_a_number(array, name)
-  "Welcome, #{name}. You are number "
-  + array.each_with_index
-  + "in line."
+def line(array)
+  if array.count == 0
+    puts "The line is currently empty."
+  else
+    puts array.each_with_index{|element, index| "#{index.to_i + 1}. #{element}"}
 end
+
+# def take_a_number(array, name)
+#   "Welcome, #{name}. You are number " + array.each_with_index{|element, index| index + 1}.to_s + " in line."
+# end
